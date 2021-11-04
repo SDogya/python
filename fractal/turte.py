40dbd17bc654586dd2e792616d2d27474b223ff7
SDogya <90337800+SDogya@users.noreply.github.com>
2021-11-04 14:36:10 +0300

    Update turte.py

--- fractal/turte.py
+++ fractal/turte.py
@@ -2,11 +2,11 @@ import cairo
 import numpy as np
 
 L = 4
-M_I = 6 # колво итераций
+M_I = 6  # колво итераций
 
 WIDTH, HEIGHT = 360, 720
-C =200   # скорсоть изменения угла отклонения
-DA = np.pi / 7 # угол отклонения
+C = 200  # скорсоть изменения угла отклонения
+DA = np.pi / 7  # угол отклонения
 
 surface = cairo.SVGSurface("example.svg", WIDTH, HEIGHT)
 context = cairo.Context(surface)
@@ -14,7 +14,7 @@ context = cairo.Context(surface)
 context.set_source_rgba(1, 0.2, 0.2, 1)
 
 
-def F(i, x, y, a, I):
+def forward(i, x, y, a, I):
     context.move_to(x, y)
     context.set_line_width(1 / I)
     nx = x + L / I * np.cos(a)
@@ -24,26 +24,26 @@ def F(i, x, y, a, I):
 
     if i < M_I:
 
-        return F(i + 1, nx, ny, a, I) + F(i + 1, nx, ny, a, I)
-    else:
-        b = [nx, ny]
-        return b
+        return forward(i + 1, nx, ny, a, I) + forward(i + 1, nx, ny, a, I)
+
+    b = [nx, ny]
+    return b
 
 
-def X(i, x, y, a):
+def func_x(i, x, y, a):
     if i < M_I:
-        b = F(i + 1, x, y, a, i + 1)
+        b = forward(i + 1, x, y, a, i + 1)
         context.move_to(x, y)
         nx = b[0]
         ny = b[1]
-        X(i + 1, nx, ny, a + DA + np.pi / (C * (i + 1)))
-        X(i + 1, nx, ny, a - DA - np.pi / (C * (i + 1)))
-        b = F(i + 1, nx, ny, a, i + 1)
+        func_x(i + 1, nx, ny, a + DA + np.pi / (C * (i + 1)))
+        func_x(i + 1, nx, ny, a - DA - np.pi / (C * (i + 1)))
+        b = forward(i + 1, nx, ny, a, i + 1)
         nx = b[0]
         ny = b[1]
-        X(i + 1, nx, ny, a)
+        func_x(i + 1, nx, ny, a)
 
 
-X(0, 180, 360, -np.pi / 2)
+func_x(0, 180, 360, -np.pi / 2)
 context.stroke()
 surface.write_to_png("example.svg")
