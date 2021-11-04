8410572ca73f601292971ee84cf01bb01056c05c
SDogya <90337800+SDogya@users.noreply.github.com>
2021-11-04 14:40:16 +0300

    rename

--- fractal/turte.py
+++ fractal/turte.py
@@ -1,11 +1,11 @@
 import cairo
 import numpy as np
 
-L = 4
+DIST = 4
 M_I = 6  # колво итераций
 
 WIDTH, HEIGHT = 360, 720
-C = 200  # скорсоть изменения угла отклонения
+SPEED = 200  # скорсоть изменения угла отклонения
 DA = np.pi / 7  # угол отклонения
 
 surface = cairo.SVGSurface("example.svg", WIDTH, HEIGHT)
@@ -17,29 +17,29 @@ context.set_source_rgba(1, 0.2, 0.2, 1)
 def forward(i, x, y, a, I):
     context.move_to(x, y)
     context.set_line_width(1 / I)
-    nx = x + L / I * np.cos(a)
-    ny = y + L / I * np.sin(a)
+    nx = x + DIST / I * np.cos(a)
+    ny = y + DIST / I * np.sin(a)
 
     context.line_to(nx, ny)
 
     if i < M_I:
         return forward(i + 1, nx, ny, a, I) + forward(i + 1, nx, ny, a, I)
 
-    b = [nx, ny]
-    return b
+    step = [nx, ny]
+    return step
 
 
 def func_x(i, x, y, a):
     if i < M_I:
-        b = forward(i + 1, x, y, a, i + 1)
+        step = forward(i + 1, x, y, a, i + 1)
         context.move_to(x, y)
-        nx = b[0]
-        ny = b[1]
-        func_x(i + 1, nx, ny, a + DA + np.pi / (C * (i + 1)))
-        func_x(i + 1, nx, ny, a - DA - np.pi / (C * (i + 1)))
-        b = forward(i + 1, nx, ny, a, i + 1)
-        nx = b[0]
-        ny = b[1]
+        nx = step[0]
+        ny = step[1]
+        func_x(i + 1, nx, ny, a + DA + np.pi / (SPEED * (i + 1)))
+        func_x(i + 1, nx, ny, a - DA - np.pi / (SPEED * (i + 1)))
+        step = forward(i + 1, nx, ny, a, i + 1)
+        nx = step[0]
+        ny = step[1]
         func_x(i + 1, nx, ny, a)
 
 
